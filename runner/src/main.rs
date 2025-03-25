a649d5241e0f741f0cad92b49d476f6f99b06ac1
Thomas Gassmann <thomas@gassmann.dev>
2025-03-26 00:37:24 +0100

    Add elf
    Signed-off-by: Thomas Gassmann <thomas@gassmann.dev>

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -5,11 +5,11 @@ use sp1_sdk::{SP1Context, SP1Prover, SP1Stdin};
 use sp1_stark::SP1ProverOpts;
 
 fn main() {
-    let mut elf = File::open("/home/thomas/git/thesis/benchmarks/bin/loop-sum/sp1/baseline").unwrap();
+    let mut elf = File::open("elf").unwrap();
     let mut buf = Vec::new();
     elf.read_to_end(&mut buf).unwrap();
     let prover = SP1Prover::<CpuProverComponents>::new();
-    let (_, pk_d, program, vk) = prover.setup(&buf);
+    let (_, pk_d, program, _) = prover.setup(&buf);
     let opts = SP1ProverOpts::auto();
     let stdin = SP1Stdin::new();
     prover
